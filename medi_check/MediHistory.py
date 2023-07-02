a58c15c8330c2bb50614c49476bc03aaa1188866
shotomorisaki <jojoto8845@icloud.com>
2023-07-02 12:40:24 +0900

    PDF Summary 7/2 12:40

--- medi_check/MediHistory.py
+++ medi_check/MediHistory.py
@@ -2,25 +2,31 @@ import streamlit as st
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 import datetime
+import pandas as pd 
+from datetime import date
 
-def soap_format():
-    # テキストファイルから情報を読み取る
-    with open('transcription.txt', 'r') as f:
-        lines = f.readlines()
+def soap_format(dataframe):
+    today = date.today()
+    return str(today.strftime("%b-%d-%Y")), str(dataframe['subjective'].iloc[0]), str(dataframe['objective'].iloc[0]), str(dataframe['assessment'].iloc[0]), str(dataframe['Plan'].iloc[0])
+
+# def soap_format():
+#     # テキストファイルから情報を読み取る
+#     with open('transcription.txt', 'r') as f:
+#         lines = f.readlines()
         
-    # テキストファイルの情報をSOAPフォーマットに整理
-    date_time, text = lines[0], lines[2:]
-    s, o, a, p = '', '', '', ''
-    for line in text:
-        if 'subjective' in line.lower():
-            s = line
-        elif 'objective' in line.lower():
-            o = line
-        elif 'assessment' in line.lower():
-            a = line
-        elif 'plan' in line.lower():
-            p = line
-    return date_time, s, o, a, p
+#     # テキストファイルの情報をSOAPフォーマットに整理
+#     date_time, text = lines[0], lines[2:]
+#     s, o, a, p = '', '', '', ''
+#     for line in text:
+#         if 'subjective' in line.lower():
+#             s = line
+#         elif 'objective' in line.lower():
+#             o = line
+#         elif 'assessment' in line.lower():
+#             a = line
+#         elif 'plan' in line.lower():
+#             p = line
+#     return date_time, s, o, a, p
 
 def create_pdf(date_time, s, o, a, p):
     c = canvas.Canvas("soap.pdf", pagesize=letter)
@@ -44,8 +50,28 @@ def create_pdf(date_time, s, o, a, p):
 
     c.save()
 
+# def create_pdf(dataframe):
+#     c = canvas.Canvas("soap.pdf", pagesize=letter)
+#     width, height = letter
+#     c.setFont("Helvetica", 12)
+
+#     y = height - 50  # start from top of page
+
+#     # Add date and time
+#     today = date.today()
+#     c.drawString(50, y, today.strftime("%b-%d-%Y"))
+#     y -= 50
+#     c.drawString(50, y, st.table(dataframe.head()))
+
+
+
+
 def main():
-    date_time, s, o, a, p = soap_format()
+    st.write("You Login as Patient-1")
+    dataframe = pd.read_csv('../SOAP/2023-category.csv')
+    st.table(dataframe.head())
+    # date_time, s, o, a, p = soap_format(dataframe)
+    date_time, s, o, a, p = soap_format(dataframe)
     create_pdf(date_time, s, o, a, p)
 
     # Add a download button for the PDF
@@ -53,8 +79,8 @@ def main():
         btn = st.download_button(
             "Download SOAP PDF",
             f,
-            file_name='soap.pdf',
-            mime='application/pdf'
+            file_name = 'soap.pdf',
+            mime = 'application/pdf'
         )
 
 main()
