3f08b758f35eb7f8e63087d7dbcf31e261aa93df
shotomorisaki <jojoto8845@icloud.com>
2023-07-02 14:02:40 +0900

    Modification 7/2 10:48 (Shoto)

--- medi_check/MediHistory.py
+++ medi_check/MediHistory.py
@@ -2,9 +2,11 @@ import streamlit as st
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 import datetime
+from datetime import date
 import os
 import base64
 import subprocess
+import pandas as pd
 
 def get_base64(bin_file):
     with open(bin_file, 'rb') as f:
@@ -66,25 +68,6 @@ def soap_format(dataframe):
     today = date.today()
     return str(today.strftime("%b-%d-%Y")), str(dataframe['subjective'].iloc[0]), str(dataframe['objective'].iloc[0]), str(dataframe['assessment'].iloc[0]), str(dataframe['Plan'].iloc[0])
 
-# def soap_format():
-#     # テキストファイルから情報を読み取る
-#     with open('transcription.txt', 'r') as f:
-#         lines = f.readlines()
-        
-#     # テキストファイルの情報をSOAPフォーマットに整理
-#     date_time, text = lines[0], lines[2:]
-#     s, o, a, p = '', '', '', ''
-#     for line in text:
-#         if 'subjective' in line.lower():
-#             s = line
-#         elif 'objective' in line.lower():
-#             o = line
-#         elif 'assessment' in line.lower():
-#             a = line
-#         elif 'plan' in line.lower():
-#             p = line
-#     return date_time, s, o, a, p
-
 def create_pdf(date_time, s, o, a, p):
     c = canvas.Canvas("soap.pdf", pagesize=letter)
     width, height = letter
@@ -107,22 +90,6 @@ def create_pdf(date_time, s, o, a, p):
 
     c.save()
 
-# def create_pdf(dataframe):
-#     c = canvas.Canvas("soap.pdf", pagesize=letter)
-#     width, height = letter
-#     c.setFont("Helvetica", 12)
-
-#     y = height - 50  # start from top of page
-
-#     # Add date and time
-#     today = date.today()
-#     c.drawString(50, y, today.strftime("%b-%d-%Y"))
-#     y -= 50
-#     c.drawString(50, y, st.table(dataframe.head()))
-
-
-
-
 def main():
     st.write("You Login as Patient-1")
     dataframe = pd.read_csv('../SOAP/2023-category.csv')
@@ -132,7 +99,6 @@ def main():
     create_pdf(date_time, s, o, a, p)
     st.title("SOAP Download")
 
-    import pandas as pd
     dataframe = pd.read_csv('../SOAP/2023-category.csv')
     st.table(dataframe)
     # Add a download button for the PDF
